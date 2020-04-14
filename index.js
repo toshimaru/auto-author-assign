97d43f999c1d0a634846a84e4e5f3bf3809ed6b8
Toshimaru <me@toshimaru.net>
2020-04-14 23:17:25 +0900

    Add github-actions test (#1)
    * Test: add who-to-greet input
    
    * Create ci.yml
    
    * Add `@zeit/ncc`
    
    * Add compiled source
    
    * Remove name and id attributes
    
    * Add name again

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const github = require('@actions/github');
 try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
