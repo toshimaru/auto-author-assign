26a0cc4ec052337d1176380ad0e16bbd15806cd2
toshimaru <me@toshimaru.net>
2020-04-15 01:52:06 +0900

    Update param

--- dist/index.js
+++ dist/index.js
@@ -515,11 +515,12 @@ try {
   const client = new github.GitHub(token);
   const number = github.context.payload.pull_request.number;
   const author = github.context.payload.pull_request.user.login;
-
-  const { owner, repo } = github.context.issue;
+  const owner = github.context.payload.repository.owner.login;
+  const repo = github.context.payload.repository.name;
   console.log(`The event owner: ${owner}`);
   console.log(`The event repo: ${repo}`);
-  client.issues.addAssignees({ owner, repo, number, author });
+
+  client.issues.addAssignees(owner, repo, number, author);
   core.info(`Added assignees to PR #${number}: ${author}`);
 } catch (error) {
   core.setFailed(error.message);
