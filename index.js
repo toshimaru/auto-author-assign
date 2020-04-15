b0499c016a93eb4faff4530ad58d1660a426dc51
toshimaru <me@toshimaru.net>
2020-04-15 10:38:29 +0900

    Fix deprecation warning
    ```
    Deprecation: [@octokit/rest] "number" parameter is deprecated for ".issues.addAssignees()". Use "issue_number" instead
    ```

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const github = require("@actions/github");
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
   const token = core.getInput("repo-token", { required: true });
   const client = new github.GitHub(token);
-  const { number, user } = github.context.payload.pull_request;
-  const author = user.login;
-  const { owner: ownerinfo, name: repo, } = github.context.payload.repository;
-  const owner = ownerinfo.login;
 
-  client.issues.addAssignees({ owner, repo, number, author });
-  core.info(`Added assignees to PR #${number}: ${author}`);
+  const { number: issue_number, user } = github.context.payload.pull_request;
+  const author = user.login;
+  const { owner: { login: owner }, name: repo, } = github.context.payload.repository;
+  // const owner = ownerinfo.login;
+
+  client.issues.addAssignees({ owner, repo, issue_number, author });
+  core.info(`Added assignees to PR #${issue_number}: ${author}`);
 } catch (error) {
   core.setFailed(error.message);
 }
