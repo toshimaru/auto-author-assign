1eef52597daac7d4d33a0be17e7c42418382db0f
Toshimaru <me@toshimaru.net>
2020-04-16 01:20:13 +0900

    Implement pull request author assign action (#2)
    * CI on pull_request
    
    * Remove who-to-greet
    
    * Set repo-token as a parameter
    
    * Update param
    
    * Refactoring: destructuring assignment
    
    ref. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
    
    * Fix assignment
    
    * addAssignees({object})
    
    * Fix deprecation warning
    
    ```
    Deprecation: [@octokit/rest] "number" parameter is deprecated for ".issues.addAssignees()". Use "issue_number" instead
    ```
    
    * Set secrets.GITHUB_TOKEN
    
    * Add debug log
    
    * Add awit and debug log
    
    ref. https://github.com/actions/toolkit/blob/master/docs/action-debugging.md
    
    * debug -> info
    
    * author -> assignees
    
    * Skips the process PR is already assigned
    
    * Re-define token
    
    * Update ci.yml

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const token = core.getInput("repo-token", { required: true });
+  const { assignees, number, user: { login: author } } = github.context.payload.pull_request;
+  const { owner: { login: owner }, name: repo, } = github.context.payload.repository;
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  if (assignees.length > 0) {
+    core.info('Skips the process to add assignees since the pull request is already assigned to someone');
+    return;
+  }
+
+  (async () => {
+    const client = new github.GitHub(token);
+    const result = await client.issues.addAssignees({ owner, repo, issue_number: number, assignees: [author] });
+    core.debug(JSON.stringify(result));
+  })();
+
+  core.info(`Added assignees to PR ${owner}/${repo}#${number}: ${author}`);
 } catch (error) {
   core.setFailed(error.message);
 }
