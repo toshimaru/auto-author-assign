7ddaf4c42548638485a43414f5783d9b31bb245e
toshimaru <me@toshimaru.net>
2020-04-16 10:25:20 +0900

    Use github.repository_owner
    ref. https://github.blog/changelog/2020-04-15-github-actions-new-workflow-features/

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require("@actions/github");
 try {
   const token = core.getInput("repo-token", { required: true });
   const { assignees, number, user: { login: author } } = github.context.payload.pull_request;
-  const { owner: { login: owner }, name: repo, } = github.context.payload.repository;
+  const { name: repo, } = github.context.payload.repository;
 
   if (assignees.length > 0) {
     core.info('Skips the process to add assignees since the pull request is already assigned to someone');
@@ -13,7 +13,7 @@ try {
 
   (async () => {
     const client = new github.GitHub(token);
-    const result = await client.issues.addAssignees({ owner, repo, issue_number: number, assignees: [author] });
+    const result = await client.issues.addAssignees({ owner: github.repository_owner, repo, issue_number: number, assignees: [author] });
     core.debug(JSON.stringify(result));
   })();
 
