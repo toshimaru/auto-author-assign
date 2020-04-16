f83c0956b6338998e6d9505a80472d9c1243376a
Toshimaru <me@toshimaru.net>
2020-04-16 10:59:41 +0900

    Use github.context.repo.{owner.repo} (#4)
    * Use github.repository_owner
    
    ref. https://github.blog/changelog/2020-04-15-github-actions-new-workflow-features/
    
    * Update log message
    
    * github.context.repository_owner
    
    * Use github.context.repo.{owner.repo}
    
    ref. https://github.com/actions/stale/blob/6127f8ef7a31ffd6d742703b46302161e932e296/src/main.ts

--- dist/index.js
+++ dist/index.js
@@ -509,7 +509,6 @@ const github = __webpack_require__(469);
 try {
   const token = core.getInput("repo-token", { required: true });
   const { assignees, number, user: { login: author } } = github.context.payload.pull_request;
-  const { owner: { login: owner }, name: repo, } = github.context.payload.repository;
 
   if (assignees.length > 0) {
     core.info('Skips the process to add assignees since the pull request is already assigned to someone');
@@ -518,11 +517,16 @@ try {
 
   (async () => {
     const client = new github.GitHub(token);
-    const result = await client.issues.addAssignees({ owner, repo, issue_number: number, assignees: [author] });
+    const result = await client.issues.addAssignees({ 
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: number,
+      assignees: [author] 
+    });
     core.debug(JSON.stringify(result));
   })();
 
-  core.info(`Added assignees to PR ${owner}/${repo}#${number}: ${author}`);
+  core.info(`@${author} has been assigned to the pull request: #${number}`);
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const github = require("@actions/github");
 try {
   const token = core.getInput("repo-token", { required: true });
   const { assignees, number, user: { login: author } } = github.context.payload.pull_request;
-  const { owner: { login: owner }, name: repo, } = github.context.payload.repository;
 
   if (assignees.length > 0) {
     core.info('Skips the process to add assignees since the pull request is already assigned to someone');
@@ -13,11 +12,16 @@ try {
 
   (async () => {
     const client = new github.GitHub(token);
-    const result = await client.issues.addAssignees({ owner, repo, issue_number: number, assignees: [author] });
+    const result = await client.issues.addAssignees({ 
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: number,
+      assignees: [author] 
+    });
     core.debug(JSON.stringify(result));
   })();
 
-  core.info(`Added assignees to PR ${owner}/${repo}#${number}: ${author}`);
+  core.info(`@${author} has been assigned to the pull request: #${number}`);
 } catch (error) {
   core.setFailed(error.message);
 }
