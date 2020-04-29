7d998e0987045d487e19d928f7d5c9077053f997
toshimaru <me@toshimaru.net>
2020-04-30 00:38:29 +0900

    Check author is bot

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
   const token = core.getInput("repo-token", { required: true });
-  const { assignees, number, user: { login: author } } = github.context.payload.pull_request;
+  const { assignees, number, user: { login: author, type } } = github.context.payload.pull_request;
 
   if (assignees.length > 0) {
     core.info('Skips the process to add assignees since the pull request is already assigned to someone');
+    return;
+  }
+
+  if (type === 'bot') {
+    core.info("Skips the process to add assignees since the author is bot");
     return;
   }
 
