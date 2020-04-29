31b1fd2623d35881f2dffc466856eeaa9f289717
Toshimaru <me@toshimaru.net>
2020-04-30 02:52:40 +0900

    Check user is bot / Check the triggered event is pull_reqeust (#6)
    * Check author is bot
    
    * Check pull_request is not undefined
    
    * Use new Error(message)
    
    * on: [pull_request]

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const github = require("@actions/github");
 
 try {
   const token = core.getInput("repo-token", { required: true });
-  const { assignees, number, user: { login: author } } = github.context.payload.pull_request;
+
+  if (github.context.payload.pull_request === undefined) {
+    throw new Error("Can't get pull_request payload. Check you trigger pull_request event");
+  }
+  const { assignees, number, user: { login: author, type } } = github.context.payload.pull_request;
 
   if (assignees.length > 0) {
-    core.info('Skips the process to add assignees since the pull request is already assigned to someone');
+    core.info(`Skips the process to add assignees since the pull request is already assigned to someone`);
+    return;
+  }
+  if (type === 'bot') {
+    core.info("Skips the process to add assignees since the author is bot");
     return;
   }
 
