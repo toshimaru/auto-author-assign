c7852399ed094f8f91e5d913e17041253cd44986
toshimaru <me@toshimaru.net>
2020-04-30 01:42:18 +0900

    Use new Error(message)

--- dist/index.js
+++ dist/index.js
@@ -507,15 +507,10 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
   const token = core.getInput("repo-token", { required: true });
 
   if (github.context.payload.pull_request === undefined) {
-    core.setFailed("Can't get pull_request payload. Check you trigger pull_request event");
-    return;
+    throw new Error("Can't get pull_request payload. Check you trigger pull_request event");
   }
   const { assignees, number, user: { login: author, type } } = github.context.payload.pull_request;
 
@@ -523,7 +518,6 @@ try {
     core.info(`Skips the process to add assignees since the pull request is already assigned to someone`);
     return;
   }
-
   if (type === 'bot') {
     core.info("Skips the process to add assignees since the author is bot");
     return;
--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
   const token = core.getInput("repo-token", { required: true });
 
   if (github.context.payload.pull_request === undefined) {
-    core.setFailed("Can't get pull_request payload. Check you trigger pull_request event");
-    return;
+    throw new Error("Can't get pull_request payload. Check you trigger pull_request event");
   }
   const { assignees, number, user: { login: author, type } } = github.context.payload.pull_request;
 
@@ -18,7 +13,6 @@ try {
     core.info(`Skips the process to add assignees since the pull request is already assigned to someone`);
     return;
   }
-
   if (type === 'bot') {
     core.info("Skips the process to add assignees since the author is bot");
     return;
